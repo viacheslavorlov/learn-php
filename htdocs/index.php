bc18f6a2e84a9c43f68d3ec2292ff910283c6828
viacheslav <viacheslavorlov@gmail.com>
2024-04-02 22:46:05 +0300

    начало ООП

---
+++ htdocs/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Оглавление</title>
+</head>
+<body>
+<main style="display: flex; flex-direction: column">
+    <a href="lesson-1/index.php"> лекция 1 начало</a>
+    <a href="lesson-2/index.php"> лекция 2</a>
+    <a href="lesson-4/index.php"> лекция 4 include</a>
+    <a href="lesson-5-variables/index.php"> лекция 5 переменные</a>
+    <a href="lesson-6-numbers/index.php"> лекция 6 числа</a>
+    <a href="lesson-7-boolean/index.php"> лекция 7 булевые значения</a>
+    <a href="lesson-8-functions/index.php"> лекция 8 встроенные функции</a>
+    <a href="lesson-9-приведение-типов/index.php"> лекция 9 приведение типов</a>
+    <a href="lesson-10-nums/index.php"> лекция 10 </a>
+    <a href="ООП-1/index.php"> ООП-1</a>
+</main>
+</body>
+</html>
+
